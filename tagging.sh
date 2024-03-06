33950614c0432f2310eb172b54ac6103b05b7447
Daison Carino <daison12006013@gmail.com>
2024-03-06 11:44:10 +0800

    Update tagging to support options

--- tagging.sh
+++ tagging.sh
@@ -9,13 +9,55 @@
 #      https://gist.github.com/JamesMGreene/cdd0ac49f90c987e45ac
 #
 # Example of this command:
-#   bash ../gittools/tagging.sh release jane v3.146.0
-#   bash ../gittools/tagging.sh hotfix "" v3.146.1
+#   bash ../gittools/tagging.sh --tag-type=release --tag-version=v3.146.0 --tag-name=jane
+#   bash ../gittools/tagging.sh --tag-type=hotfix  --tag-version=v3.146.0
 #=======================================================================
 
-mainBranch="master"
-devBranch="develop"
-origin="origin"
+usage="$(basename "$0") [-h] [-e] [-f] [--help] [--origin <origin>] [--main-branch <branch>] [--develop-branch <branch>] [--tag-type <type>] [--tag-name <name>] [--tag-version <version>] -- script to help generate the console command to deploy release or hotfix
+
+where:
+    --origin <origin>           Specify the origin (default: origin)
+    --main-branch <branch>      Specify the main branch (default: master)
+    --develop-branch <branch>   Specify the develop branch (default: develop)
+    --tag-type <type>           Specify the tag type (default: release)
+    --tag-name <name>           Specify the tag name (default: \"\")
+    --tag-version <version>     Specify the tag version (default: \"\")
+    -e, --execute               Execute the generated console command
+    -h, --help                  Show this help text"
+
+MAIN_BRANCH="master";
+DEVELOP_BRANCH="develop";
+ORIGIN="origin";
+EXECUTE=0;
+TAG_TYPE="release"; # or hotfix
+TAG_NAME=""; # release name
+TAG_VERSION=""; # release version
+
+# parse the options
+while getopts "eh-:" OPT ; do
+    case ${OPT} in
+        -)
+            case ${OPTARG} in
+                main-branch=*) MAIN_BRANCH=${OPTARG#*=} ;;
+                develop-branch=*) DEVELOP_BRANCH=${OPTARG#*=} ;;
+                tag-type=*) TAG_TYPE=${OPTARG#*=} ;;
+                tag-name=*) TAG_NAME=${OPTARG#*=} ;;
+                tag-version=*) TAG_VERSION=${OPTARG#*=} ;;
+                execute) EXECUTE=1 ;;
+                origin=*) ORIGIN=${OPTARG#*=} ;;
+                help) echo "${usage}"; exit 0 ;;
+                *) echo "${usage}"; exit 1 ;;
+            esac
+            ;;
+        e) EXECUTE=1 ;;
+        h) echo "${usage}"; exit 0 ;;
+        *) echo "${usage}"; exit 1 ;;
+    esac
+done
+
+# Append subsequent options from external
+shift $((OPTIND-1))
+EXTERNAL_OPTIONS="$@"
 
 function dirty() {
     if [ $(git diff --name-only) ]; then
@@ -28,85 +70,107 @@ function dirty() {
 
 function print() {
 
+    # Check if the tag type is not release or hotfix
     if [ "$1" != "release" ] && [ "$1" != "hotfix" ]; then
-        echo "First argument must be release or hotfix!"
+        echo "Provide a valid --tag-type= (release or hotfix)"
+        exit 1
+    fi
+
+    # Check if the tag type is release and the tag name is empty
+    if [ "$1" == "release" ] && [ "$2" == "" ]; then
+        echo "Provide a valid --tag-name= (e.g. jane)"
         exit 1
     fi
 
     if [ -z $3 ]; then
-        echo "Provide the second param as the TAG version"
+        echo "Provide the --tag-version= (e.g. v3.146.0)"
         exit 1
     fi
 
+    local FINAL_SCRIPT=""
+
     if [ "$1" = "release" ]; then
-    echo "
-git fetch
-git checkout release/$2
-git branch $mainBranch -D
-git branch $devBranch -D
+    FINAL_SCRIPT+="
+echo \"Cleaning up local branches...\" && \\
+git fetch && \\
+git checkout $1/$2 && \\
+git branch $MAIN_BRANCH -D && \\
+git branch $DEVELOP_BRANCH -D && \\
+echo \"Cleaning up local branches... done\"
 
-git fetch
-git checkout $mainBranch
-git checkout $devBranch
-git pull $origin $devBranch --no-edit
-git pull $origin $1/$2 --no-edit
-git push $origin $devBranch
+echo \"Updating [$DEVELOP_BRANCH] branch and push to [$ORIGIN]...\" && \\
+git fetch && \\
+git checkout $MAIN_BRANCH && \\
+git checkout $DEVELOP_BRANCH && \\
+git pull $ORIGIN $DEVELOP_BRANCH --no-edit && \\
+git pull $ORIGIN $1/$2 --no-edit && \\
+git push $ORIGIN $DEVELOP_BRANCH && \\
+echo \"Updating [$DEVELOP_BRANCH] branch and push to [$ORIGIN]... done\"
 
-git branch | grep $1\* | xargs git branch -D
-git tag -l | xargs git tag -d
-git fetch --tags
+echo \"Cleaning up local tags\" && \\
+git branch | grep $1\* | xargs git branch -D && \\
+git tag -l | xargs git tag -d && \\
+git fetch --tags && \\
+echo \"Cleaning up local tags... done\"
 
-git flow $1 start $3
-git flow $1 publish $3
 "
     fi
 
     if [ "$1" = "hotfix" ]; then
-    echo "
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Make sure you've created a branch hotfix/$3
-git checkout develop
-git branch master -D
-git fetch
-git checkout master
-git branch hotfix/$3
-git checkout hotfix/$3
-git push origin hotfix/$3
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    FINAL_SCRIPT+="
+echo \"Cleaning up local branches...\" && \\
+git fetch && \\
+git checkout $1/$3 && \\
+git branch $MAIN_BRANCH -D && \\
+git branch $DEVELOP_BRANCH -D && \\
+git branch $1/$3 -D && \\
+echo \"Cleaning up local branches... done\"
 
-git fetch
-git branch $mainBranch -D
-git branch $devBranch -D
-git branch $1/$3 -D
+git fetch && \\
+git checkout $MAIN_BRANCH && \\
+git checkout $DEVELOP_BRANCH && \\
+git checkout $1/$3 && \\
 
-git fetch
-git checkout $mainBranch
-git checkout $devBranch
-git checkout $1/$3
+echo \"Cleaning up local tags\" && \\
+git branch | grep $1\* | xargs git branch -D && \\
+git tag -l | xargs git tag -d && \\
+git fetch --tags && \\
+echo \"Cleaning up local tags... done\"
 
-git tag -l | xargs git tag -d
-git fetch --tags
 "
     fi
 
-    echo "
-git checkout $mainBranch
-git merge --no-ff --no-edit --no-verify $1/$3
-git tag -a $3 -m "$3"
-git checkout $devBranch
-git merge --no-ff --no-edit --no-verify $3
-git push origin $devBranch
-git push origin $mainBranch
-git push origin --tags
-git branch -d $1/$3
+    FINAL_SCRIPT+="echo \"Creating tag\" && \\
+git checkout $MAIN_BRANCH && \\
+git merge --no-ff --no-edit --no-verify $1/$3 && \\
+git tag -a $3 -m "$3" && \\
+git checkout $DEVELOP_BRANCH && \\
+git merge --no-ff --no-edit --no-verify $3 && \\
+git push $ORIGIN $DEVELOP_BRANCH && \\
+git push $ORIGIN $MAIN_BRANCH && \\
+git push $ORIGIN --tags && \\
+echo \"Creating tag... done\"
+
 "
 
     if [ "$1" = "release" ]; then
-    echo "
-git push origin -d $1/$3
+        FINAL_SCRIPT+="git branch -d $1/$3 && \\
+git push $ORIGIN -d $1/$3
 "
     fi
+
+    echo "$FINAL_SCRIPT"
 }
 
 dirty
-print "$1" "$2" "$3"
+result=$(print "$TAG_TYPE" "$TAG_NAME" "$TAG_VERSION")
+
+if [ $EXECUTE -eq 1 ]; then
+    echo "Executing the generated console command..."
+    eval "$result"
+else
+    echo "Generated console command:"
+    echo "$result
+"
+
+fi
