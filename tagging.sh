2f92fa859c3f0eb7ce0a7b8b9e08d23ed3344d61
Daison Carino <daison12006013@gmail.com>
2024-03-06 14:04:21 +0800

    Add git-url option

--- tagging.sh
+++ tagging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#=======================================================================
+#========================================================================================================================================
 # This command is used to help us generate the
 # console command to deploy release or hotfix
 #
@@ -9,11 +9,11 @@
 #      https://gist.github.com/JamesMGreene/cdd0ac49f90c987e45ac
 #
 # Example of this command:
-#   bash ../gittools/tagging.sh --tag-type=release --tag-version=v3.146.0 --tag-name=jane
-#   bash ../gittools/tagging.sh --tag-type=hotfix  --tag-version=v3.146.0
-#=======================================================================
+#   bash ../gittools/tagging.sh --git-url=https://bitbucket.org/meta/react --tag-type=release --tag-version=v3.146.0 --tag-name=jane
+#   bash ../gittools/tagging.sh --git-url=https://bitbucket.org/meta/react --tag-type=hotfix  --tag-version=v3.146.0
+#========================================================================================================================================
 
-usage="$(basename "$0") [-h] [-e] [-f] [--help] [--origin <origin>] [--main-branch <branch>] [--develop-branch <branch>] [--tag-type <type>] [--tag-name <name>] [--tag-version <version>] -- script to help generate the console command to deploy release or hotfix
+usage="$(basename "$0") [-h] [-e] [-f] [--help] [--origin <origin>] [--main-branch <branch>] [--develop-branch <branch>] [--tag-type <type>] [--tag-name <name>] [--tag-version <version>] [--git-url <url>] -- script to help generate the console command to deploy release or hotfix
 
 where:
     --origin <origin>           Specify the origin (default: origin)
@@ -22,6 +22,7 @@ where:
     --tag-type <type>           Specify the tag type (default: release)
     --tag-name <name>           Specify the tag name (default: \"\")
     --tag-version <version>     Specify the tag version (default: \"\")
+    --git-url <url>             Specify the repository URL e.g "https://bitbucket.org/meta/react"
     -e, --execute               Execute the generated console command
     -h, --help                  Show this help text"
 
@@ -32,6 +33,7 @@ EXECUTE=0;
 TAG_TYPE="release"; # or hotfix
 TAG_NAME=""; # release name
 TAG_VERSION=""; # release version
+GIT_URL=""; # repository url
 
 # parse the options
 while getopts "eh-:" OPT ; do
@@ -43,6 +45,7 @@ while getopts "eh-:" OPT ; do
                 tag-type=*) TAG_TYPE=${OPTARG#*=} ;;
                 tag-name=*) TAG_NAME=${OPTARG#*=} ;;
                 tag-version=*) TAG_VERSION=${OPTARG#*=} ;;
+                git-url=*) GIT_URL=${OPTARG#*=} ;;
                 execute) EXECUTE=1 ;;
                 origin=*) ORIGIN=${OPTARG#*=} ;;
                 help) echo "${usage}"; exit 0 ;;
@@ -96,82 +99,82 @@ function print() {
 
     if [ "$1" = "release" ]; then
         SCRIPT_CLEANUP_LOCAL_BRANCHES="
-            echo \"Cleaning up local branches\" && \\
-            git fetch && \\
-            git checkout $1/$2 && \\
-            git branch $MAIN_BRANCH -D && \\
-            git branch $DEVELOP_BRANCH -D && \\
-            echo \"Cleaning up local branches... done\"
+            echo \"[git-flow] Cleaning up local branches\"
+            git fetch
+            git checkout $1/$2
+            git branch $MAIN_BRANCH -D
+            git branch $DEVELOP_BRANCH -D
+            echo \"[git-flow] Cleaning up local branches... done\"
         "
 
         SCRIPT_UPDATE_DEV_BR="
-            echo \"Updating [$DEVELOP_BRANCH] branch and push to [$ORIGIN]\" && \\
-            git fetch && \\
-            git checkout $MAIN_BRANCH && \\
-            git checkout $DEVELOP_BRANCH && \\
-            git pull $ORIGIN $DEVELOP_BRANCH --no-edit && \\
-            git pull $ORIGIN $1/$2 --no-edit && \\
-            git push $ORIGIN $DEVELOP_BRANCH && \\
-            echo \"Updating [$DEVELOP_BRANCH] branch and push to [$ORIGIN]... done\"
+            echo \"[git-flow] Updating [$DEVELOP_BRANCH] branch and push to [$ORIGIN]\"
+            git fetch
+            git checkout $MAIN_BRANCH
+            git checkout $DEVELOP_BRANCH
+            git pull $ORIGIN $DEVELOP_BRANCH --no-edit
+            git pull $ORIGIN $1/$2 --no-edit
+            git push $ORIGIN $DEVELOP_BRANCH
+            echo \"[git-flow] Updating [$DEVELOP_BRANCH] branch and push to [$ORIGIN]... done\"
         "
 
         SCRIPT_CLEANUP_LOCAL_TAGS="
-            echo \"Cleaning up local tags\" && \\
-            git branch | grep $1\* | xargs git branch -D && \\
-            git tag -l | xargs git tag -d && \\
-            git fetch --tags && \\
-            echo \"Cleaning up local tags... done\"
+            echo \"[git-flow] Cleaning up local tags\"
+            git branch | grep $1\* | xargs git branch -D
+            git tag -l | xargs git tag -d
+            git fetch --tags
+            echo \"[git-flow] Cleaning up local tags... done\"
         "
     fi
 
     if [ "$1" = "hotfix" ]; then
         SCRIPT_CLEANUP_LOCAL_BRANCHES="
-            echo \"Cleaning up local branches\" && \\
-            git fetch && \\
-            git checkout $1/$3 && \\
-            git branch $MAIN_BRANCH -D && \\
-            git branch $DEVELOP_BRANCH -D && \\
-            git branch $1/$3 -D && \\
-            echo \"Cleaning up local branches... done\"
+            echo \"[git-flow] Cleaning up local branches\"
+            git fetch
+            git checkout $1/$3
+            git branch $MAIN_BRANCH -D
+            git branch $DEVELOP_BRANCH -D
+            git branch $1/$3 -D
+            echo \"[git-flow] Cleaning up local branches... done\"
         "
 
         SCRIPT_CHECKOUT_TYPE_BRANCH="
-            echo \"Cleaning up local branches\"
-            git fetch && \\
-            git checkout $MAIN_BRANCH && \\
-            git checkout $DEVELOP_BRANCH && \\
-            git checkout $1/$3 && \\
-            echo \"Cleaning up local branches... done\"
+            echo \"[git-flow] Cleaning up local branches\"
+            git fetch
+            git checkout $MAIN_BRANCH
+            git checkout $DEVELOP_BRANCH
+            git checkout $1/$3
+            echo \"[git-flow] Cleaning up local branches... done\"
         "
 
         SCRIPT_CLEANUP_LOCAL_TAGS="
-            echo \"Cleaning up local tags\" && \\
-            git branch | grep $1\* | xargs git branch -D && \\
-            git tag -l | xargs git tag -d && \\
-            git fetch --tags && \\
-            echo \"Cleaning up local tags... done\"
+            echo \"[git-flow] Cleaning up local tags\"
+            git branch | grep $1\* | xargs git branch -D
+            git tag -l | xargs git tag -d
+            git fetch --tags
+            echo \"[git-flow] Cleaning up local tags... done\"
         "
     fi
 
     SCRIPT_CREATE_TAG="
-        echo \"Creating tag\" && \\
-        git checkout $MAIN_BRANCH && \\
-        git merge --no-ff --no-edit --no-verify $1/$3 && \\
-        git tag -a $3 -m "$3" && \\
-        git checkout $DEVELOP_BRANCH && \\
-        git merge --no-ff --no-edit --no-verify $3 && \\
-        git push $ORIGIN $DEVELOP_BRANCH && \\
-        git push $ORIGIN $MAIN_BRANCH && \\
-        git push $ORIGIN --tags && \\
-        echo \"Creating tag... done\"
+        echo \"[git-flow] Creating tag\"
+        git checkout $MAIN_BRANCH
+        git merge --no-ff --no-edit --no-verify $1/$3
+        git tag -a $3 -m "$3"
+        git checkout $DEVELOP_BRANCH
+        git merge --no-ff --no-edit --no-verify $3
+        git push $ORIGIN $DEVELOP_BRANCH
+        git push $ORIGIN $MAIN_BRANCH
+        git push $ORIGIN --tags
+        echo \"[git-flow] Creating tag... done\"
     "
 
     if [ "$1" = "release" ]; then
         SCRIPT_CLEANUP_TYPE_BRANCH="
-            echo \"Cleaning up [$1/$3] branch\" && \\
-            git branch -d $1/$3 && \\
+            echo \"[git-flow] Cleaning up [$1/$3] branch\"
+            git branch -d $1/$3
             git push $ORIGIN -d $1/$3
-            echo \"Cleaning up [$1/$3] branch... done\"
+            echo \"[git-flow] Cleaning up [$1/$3] branch... done\"
         "
     fi
 
@@ -199,3 +202,20 @@ else
     echo "$result
 "
 fi
+
+# check if GIT_URL is bitbucket.org
+if [[ $GIT_URL == *"bitbucket.org"* ]]; then
+    echo "
+    Diff can be seen here: ${GIT_URL}/branches/compare/${TAG_VERSION}...${MAIN_BRANCH}#diff
+"
+elif [[ $GIT_URL == *"github.com"* ]]; then
+    echo "
+    Diff can be seen here: ${GIT_URL}/compare/${MAIN_BRANCH}...${TAG_VERSION}
+"
+elif [[ $GIT_URL == *"gitlab.com"* ]]; then
+    echo "
+    Diff can be seen here: ${GIT_URL}/-/compare/${MAIN_BRANCH}...${TAG_VERSION}
+"
+fi
+
+
